17d8e12a66533632c94efefccaf11b1b829c239b
Mainro Code <112573027+Romain-Constant@users.noreply.github.com>
2023-02-16 15:11:25 +0100

    new

---
+++ 6kyu/bit-counting-6kyu/exercice.js
@@ -0,0 +1,7 @@
+/* Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case */
+
+var countBits = function(n) {
+  return Number(n.toString(2).split("").reduce((acc, curr) => (acc * 1 + curr * 1) ))    
+}
--- 7kyu/descending-order-7kyu/exercice.js
+++ 7kyu/descending-order-7kyu/exercice.js
@@ -1,13 +1,12 @@
-/* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+/* Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+Examples:
+Input: 42145 Output: 54421
 
-Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-35)
+Input: 145263 Output: 654321
 
-Note: The function accepts an integer and returns an integer.
+Input: 123456789 Output: 987654321 */
 
-Happy Coding! */
-
-function squareDigits(num){
-  return Number(num.toString().split("").map(x => x * x).join(""))
+function descendingOrder(n){
+  return Number(n.toString().split("").sort().reverse().join(""))
 }

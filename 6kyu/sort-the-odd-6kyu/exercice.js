d57e5f8cc33761bfe674e1656d2565ef0bfb11dd
Romain-Constant <romain.constant59@gmail.com>
2023-03-22 13:25:20 +0100

    Update exercice.js

--- 6kyu/sort-the-odd-6kyu/exercice.js
+++ 6kyu/sort-the-odd-6kyu/exercice.js
@@ -1,29 +1,18 @@
-/* Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+/* Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
-Examples:
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0] */
 
-* 'abc' =>  ['ab', 'c_']
-* 'abcdef' => ['ab', 'cd', 'ef'] */
+function sortArray(array) {
+    let returnArray = array.filter((number) => number % 2 !== 0).sort((a, b) => a - b);
 
-function solution(str) {
-    let returnArray = [];
-    if (str === "") {
-        return returnArray;
-    }
-    else if (str.length % 2 === 0) {
-        for (let i = 0; i < str.length; i += 2) {
-            returnArray.push(str[i] + str[i + 1]);
-        }
-    }
-    else {
-        for (let i = 0; i < str.length; i += 2) {
-            if (i === str.length - 1) {
-                returnArray.push(str[i] + "_");
-            }
-            else {
-                returnArray.push(str[i] + str[i + 1]);
-            }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            returnArray.splice(i, 0, array[i]);
         }
     }
     return returnArray;
-}
+};
